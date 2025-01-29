db111a63ae7be25f6f4de79802f259c968a2ec6c
samudrababai <147082816+samudrababai@users.noreply.github.com>
2025-01-29 18:17:06 +0530

    implemented courseSection and courseCard component:

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,12 +1,27 @@
-import  { createContext} from "react";
+import  { createContext,useEffect,useState} from "react";
+import { dummyCourses } from "../assets/assets";
 
 // Create a Context
 export const AppContext = createContext();
 
 // Create a provider component
 export const AppContextProvider = (props) => {
+
+  const  currency = import.meta.env.VITE_CURRENCY
+  const [allCourses,setAllCourses] = useState([])
+
+  // Fetch courses data
+   const fetchAllCourses = async () => {
+      setAllCourses(dummyCourses)
+   }
+   useEffect(() => {
+    fetchAllCourses()
+   }, [])
+
     const value = {
-       
+      currency,
+      allCourses,
+      fetchAllCourses
     }
   
 
